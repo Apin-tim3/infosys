60a9fea847d02bb925c62a57a1e2bcc5c8058be0
rindo789 <rindo.ondrejka@gmail.com>
2021-01-09 16:31:48 +0100

    Update home.php

--- Domov/home.php
+++ Domov/home.php
@@ -1,21 +1,21 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="UTF-8">
-        <link rel="stylesheet" href="style.css">
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="domstyle.css">
         <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     </head>
     <body>
         <div id="sidebar">
             <a href="home.php"><button id="home_button">Domov</button></a>
             <a href="../Rozvrh/Rozvrh.html"><button id="rozvrh">Rozvrh</button></a>
-            <button id="zapis">Zápis</button>
+            <a href="../zapisy/zapis.html"><button id="zapis">Zápis</button></a>
             <button id="prehlad">Prehľad štúdia</button>
         </div>
 
         <div id='user'>
-            <img src='images/message.png'>
-            <img src='images/open.png'>
+            <img src='../images/message.png'>
+            <img src='../images/open.png'>
 
         <?php include 'funckie.php';
             LoginName();?>
@@ -38,17 +38,11 @@
             GenerateClassNow();
             ?>
             
-                <!-- 
-                <p>Hodina</p>
-                <p>Miestnost</p>-->
-                <h2>Ďaľšia hodina:</h2>
-                <?php 
-                GenerateClassLater();
-                ?>
-                <!--<p>Hodina</p>
-                <p>Čas</p>
-                <p>Miestnosť</p>
-                 -->
+            <h2>Ďaľšia hodina:</h2>
+            <?php 
+            GenerateClassLater();
+            ?>
+
             </div>
             <div id="home_rozvrh">
                 <h1>Dnešný rozvrh</h1>
@@ -56,26 +50,15 @@
 
                 <!--Tabuľka sa bude automaticky generovať, toto je len preview-->
                 <table>
-                    <tr>
-                        <th>8:25 - 10:15</td>
+                    <!--<tr>
+                        <th>8:25 - 10:15</th>
                         <td class="predmet"><div class="hodina"><p>Hodina</p><p>Miestnosť</p></div></td>
-                    </tr>
-                    <tr>
-                        <th>10:15 - 12:05</td>
-                        <td class="predmet"><div class="hodina"><p>Hodina</p><p>Miestnosť</p></div></td>
-                    </tr>
-                    <tr>
-                        <th>12:05 - 13:55</td>
-                        <td class="predmet"><div class="hodina"><p>Hodina</p><p>Miestnosť</p></div></td>
-                    </tr>
-                    <tr>
-                        <th>13:55 - 15:45</td>
-                        <td class="predmet"><div class="hodina"><p>Hodina</p><p>Miestnosť</p></div></td>
-                    </tr>
-                    <tr>
-                        <th>15:45 - 17:35</td>
-                        <td class="predmet"><div class="hodina"><p>Hodina</p><p>Miestnosť</p></div></td>
-                    </tr>
+                    </tr>-->
+
+                    <?php
+                    GenerateRozvrhHome()
+                    ?>
+
                 </table>
             </div>
             <div id="oznamy">
